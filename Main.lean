d731c0a5d70d95a7fbeb5c7a259740fbf8839f3c
bernborgess <bernborgess@outlook.com>
2024-04-15 17:01:43 +0000

    OPB Parser: Reading file

--- Main.lean
+++ Main.lean
@@ -1,4 +1,24 @@
 import «LeanCuttingPlanes»
 
-def main : IO Unit :=
-  IO.println s!"Hello, World!"
+def main : IO Unit := do
+  let stdin ← IO.getStdin
+  IO.print s!"Enter the filename: "
+  let line ← stdin.getLine
+  let path := s!"example/{line.trim}.opb"
+  let fileExists ← System.FilePath.pathExists path
+
+  if ! fileExists then
+    IO.println s!"File {line.trim}.opb does not exist in the example folder!"
+    return ()
+
+  IO.println s!"Opening {path}"
+  let file ← IO.FS.readFile path
+
+  IO.println s!"This is your file: "
+  IO.println s!"#######################################"
+  IO.println file
+  IO.println s!"#######################################"
+
+  IO.println "Detecting counters..."
+  -- * #variable= {v} #constraint= {c}
+  IO.println "osh"
--- run.sh
+++ run.sh
@@ -1,1 +1,10 @@
+#!/bin/bash
+
+# Exit immediately if any command has a non-zero exit status.
+set -e
+
+lake build
+
+clear
+
 ./.lake/build/bin/lean-cutting-planes
