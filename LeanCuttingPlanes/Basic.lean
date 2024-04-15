20c5b8251ba4d33400a8298b7257af1b852a5a88
bernborgess <bernborgess@outlook.com>
2024-04-15 16:21:02 +0000

    Basic Representation

--- LeanCuttingPlanes/Basic.lean
+++ LeanCuttingPlanes/Basic.lean
@@ -1,1 +1,41 @@
-def hello := "world"
+/-! # Representation of a Linear Inequality
+
+  ## Basic Definition
+
+  As stated in [Jakob Nordstrom's talk](https://jakobnordstrom.se/docs/presentations/TalkSimons2102_PseudoBooleanSolvingLiveTalk.pdf),
+  Pseudo-Boolean constraints are 0-1 integer linear constraints
+    `  ∑ᵢ aᵢlᵢ ⋈ A `
+
+  With the following components
+
+ * Coefficients aᵢ
+ Are integers ℤ that represent some weight of each variable in our problem
+
+ * Variables lᵢ
+ Are integers either 0 or 1, that represent whether we set or not that variable
+
+ * Constant A
+ Is an integer ℤ that represents our constraint
+
+ * Operator ⋈
+ Is the relation that serves our inequality, such that ⋈ ∈ {≥,≤,=,>,<}.
+
+
+  ## Normalized Form
+
+  To perform the proofs about linear inequalities it is much simpler to set a normal form
+  that can represent all other inequalities and prove theorems about it, without any loss
+  of generality.
+
+  In this library, we have settled in the following notation:
+  pseudo-Boolean PBO format (only linear objective and constraints)
+
+  * ⋈ is always ≥
+  * aᵢ is a list of ℤ
+  * A is a ℤ
+
+
+
+
+
+-/
--- Main.lean
+++ Main.lean
@@ -1,4 +1,4 @@
 import «LeanCuttingPlanes»
 
 def main : IO Unit :=
-  IO.println s!"Hello, {hello}!"
+  IO.println s!"Hello, World!"
