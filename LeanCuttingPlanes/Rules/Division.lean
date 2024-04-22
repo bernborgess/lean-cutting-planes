073e4e888281abeeaea33b832daff61ffb42e802
bernborgess <bernborgess@outlook.com>
2024-04-22 14:30:56 -0300

    Division Rule Statment, whnf timeout Line 21: (deterministic) timeout at 'whnf', maximum number of heartbeats (200000) has been reached

--- LeanCuttingPlanes/Rules/Division.lean
+++ LeanCuttingPlanes/Rules/Division.lean
@@ -1,6 +1,9 @@
 import «LeanCuttingPlanes».Data.PseudoBoolean
 
-open PseudoBoolean
+namespace PseudoBoolean
+open FinVec
+
+def ceildiv (c : ℕ) (a : ℤ) := (a+c-1) / c
 
 -- Division
 -- ∑i (a i * l i) ≥ A
@@ -8,3 +11,14 @@ open PseudoBoolean
 -- c > 0
 -- ⊢
 -- ∑i (ceil(a i / c) * l i) ≥ ceil(A / c)
+theorem Division {as : Fin n → ℤ} {A : ℤ} (c : ℕ) (hc0 : c > 0)
+  (ha : PBProp as A)
+  : PBProp (map (ceildiv c) as) (ceildiv c A) := sorry
+
+example (ha : PBProp ![3,4] 3)
+  : PBProp ![2,2] 2 := by
+  let h2z : 2 > 0 := Nat.zero_lt_succ 1
+  exact Division 2 h2z ha
+  done
+
+end PseudoBoolean
