8420e2067a92b1b9e38324a86ca2b0c3919251ff
bernborgess <bernborgess@outlook.com>
2024-04-22 13:46:03 -0300

    Multiplication Rule Statement

--- LeanCuttingPlanes/Rules/Multiplication.lean
+++ LeanCuttingPlanes/Rules/Multiplication.lean
@@ -1,6 +1,6 @@
 import «LeanCuttingPlanes».Data.PseudoBoolean
 
-open PseudoBoolean
+namespace PseudoBoolean
 
 -- Multiplication
 -- ∑i (a i * l i) ≥ A
@@ -8,3 +8,14 @@ open PseudoBoolean
 -- c > 0
 -- ⊢
 -- ∑i (c * a i * l i) ≥ c*A
+theorem Multiplication {as : Fin n → ℤ} {A : ℤ} {c : ℕ} (hc0 : c > 0)
+  (ha : PBProp as A)
+  : PBProp (as * c : Fin n → ℤ) (A * c) := sorry
+
+example (ha : PBProp ![1,0] 3)
+  : PBProp ![2,0] 6 := by
+  let h2z : 2 > 0 := Nat.zero_lt_succ 1
+  exact Multiplication h2z ha
+  done
+
+end PseudoBoolean
