9e81609ceea612e00b94d9949bd40bb85df5662f
bernborgess <bernborgess@outlook.com>
2024-04-22 13:08:54 -0300

    Added PBProp definition

--- LeanCuttingPlanes/Data/PseudoBoolean.lean
+++ LeanCuttingPlanes/Data/PseudoBoolean.lean
@@ -10,31 +10,28 @@ def PBInequality {n : ℕ} (cs : Fin n → ℤ) (xs : Fin n → Fin 2) (const : 
   ∑ i, (cs i * xs i) ≥ const
 
 example : PBInequality ![1] ![1] 1 := by
-  -- Expand the goal to 1 * 1 ≥ 1
-  reduce
-  -- Prove that 1 * 1 ≥ 1
-  exact Int.le_refl 1
+  reduce                -- Expand the goal to 1 * 1 ≥ 1
+  exact Int.le_refl 1   -- Prove that 1 * 1 ≥ 1
   done
 
 example : PBInequality ![1,2] ![0,1] 2 := by
-  -- Change goal to 1 * 0 + 2 * 1 ≥ 2
-  reduce
-  -- Prove 1 * 0 + 2 * 1 ≥ 2
-  exact Int.le_refl 2
+  reduce                -- Change goal to 1 * 0 + 2 * 1 ≥ 2
+  exact Int.le_refl 2   -- Prove 1 * 0 + 2 * 1 ≥ 2
   done
 
-theorem t1 : PBInequality ![3,4] ![0,1] 2 := by
+example : PBInequality ![3,4] ![0,1] 2 := by
   reduce
   exact Int.NonNeg.mk 2
   done
 
-example : ∃xs, PBInequality ![3,4] xs 2 :=
+def PBProp (cs : Fin n → ℤ) (const : Int) := ∃xs, PBInequality cs xs const
+
+example : PBProp ![3,4] 2 :=
   let xs := ![0,1]
-  let proof : PBInequality ![3, 4] xs 2 := by
+  let proof : PBInequality ![3,4] xs 2 := by
     reduce
     exact Int.NonNeg.mk 2
     done
   ⟨xs,proof⟩
-
 
 end PseudoBoolean
