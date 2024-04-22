57f7c24aefe7d9459612f041deda9c0bd6ad48a2
bernborgess <bernborgess@outlook.com>
2024-04-22 14:00:03 -0300

    Saturation Rule Statement

--- LeanCuttingPlanes/Rules/Saturation.lean
+++ LeanCuttingPlanes/Rules/Saturation.lean
@@ -1,8 +1,19 @@
 import «LeanCuttingPlanes».Data.PseudoBoolean
 
-open PseudoBoolean
+namespace PseudoBoolean
+open FinVec
 
 -- Saturation
 -- ∑i (a i * l i) ≥ A
 -- ⊢
 -- ∑i ( min(a i, A) * l i) ≥ A
+theorem Saturation {as : Fin n → ℤ} {A : Int}
+  (ha : PBProp as A)
+  : PBProp (map (min A) as) A := sorry
+
+example (ha : PBProp ![3,4] 3)
+  : PBProp ![3,3] 3 := by
+  exact Saturation ha
+  done
+
+end PseudoBoolean
