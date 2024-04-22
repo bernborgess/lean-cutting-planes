39ac1cbef1239366ce09581676dd2500a16a7b69
bernborgess <bernborgess@outlook.com>
2024-04-22 14:03:10 -0300

    Renaming for clarity

--- LeanCuttingPlanes/Rules/Addition.lean
+++ LeanCuttingPlanes/Rules/Addition.lean
@@ -7,10 +7,10 @@ namespace PseudoBoolean
 -- ∑i (b i * l i) ≥ B
 -- ⊢
 -- ∑i ((a i + b i) * l i) ≥ A + B
-theorem Addition {as bs : Fin n → ℤ} {ak bk: Int}
-  (ha : PBProp as ak)
-  (hb : PBProp bs bk)
-  : PBProp (as + bs) (ak + bk) := sorry
+theorem Addition {as bs : Fin n → ℤ} {A B: Int}
+  (ha : PBProp as A)
+  (hb : PBProp bs B)
+  : PBProp (as + bs) (A + B) := sorry
 
 example (ha : PBProp ![1,0] 1) (hb : PBProp ![1,1] 2)
   : PBProp (![1,0] + ![1,1]) (1 + 2) := by
