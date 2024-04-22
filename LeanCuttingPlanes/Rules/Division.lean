04612d461655f69910674109b24cca775042ea5b
bernborgess <bernborgess@outlook.com>
2024-04-22 16:20:46 -0300

    Fix whnf timeout removing the implicit from the finset `as`

--- LeanCuttingPlanes/Rules/Division.lean
+++ LeanCuttingPlanes/Rules/Division.lean
@@ -11,14 +11,14 @@ def ceildiv (c : ℕ) (a : ℤ) := (a+c-1) / c
 -- c > 0
 -- ⊢
 -- ∑i (ceil(a i / c) * l i) ≥ ceil(A / c)
-theorem Division {as : Fin n → ℤ} {A : ℤ} (c : ℕ) (hc0 : c > 0)
+theorem Division (as : Fin n → ℤ) {A : ℤ} {c : ℕ} (hc0 : c > 0)
   (ha : PBProp as A)
   : PBProp (map (ceildiv c) as) (ceildiv c A) := sorry
 
 example (ha : PBProp ![3,4] 3)
   : PBProp ![2,2] 2 := by
   let h2z : 2 > 0 := Nat.zero_lt_succ 1
-  exact Division 2 h2z ha
+  exact Division ![3,4] h2z ha
   done
 
 end PseudoBoolean
