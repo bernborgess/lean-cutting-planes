a269a3050ca2ea2926c5ee8f429df15fd1a2b11a
bernborgess <bernborgess@outlook.com>
2024-04-23 17:56:06 -0300

    Extended example with `derived` and `resolved`

--- Main.lean
+++ Main.lean
@@ -7,12 +7,12 @@ def pb : PBIneq ![1,2] ![0,1] 1 := by
   exact Int.NonNeg.mk 1
   done
 
-variable {xs : Fin 4 → Fin 2}
-
-example (c1 : PBIneq ![1,2,1,0] xs 2)
-        (c2 : PBIneq ![1,2,4,2] xs 5)
-        (c3 : PBIneq ![0,0,0,-1] xs (-1))
-        : PBIneq ![1,2,2,0] xs 3
+def derived
+  (xs : Fin 4 → Fin 2)
+  (c1 : PBIneq ![1,2,1,0] xs 2)
+  (c2 : PBIneq ![1,2,4,2] xs 5)
+  (c3 : PBIneq ![0,0,0,-1] xs (-1))
+  : PBIneq ![1,2,2,0] xs 3
   := by
   let h2z : 2 > 0 := Nat.zero_lt_succ 1
   let h3z : 3 > 0 := Nat.zero_lt_succ 2
@@ -22,6 +22,13 @@ example (c1 : PBIneq ![1,2,1,0] xs 2)
   let t4 : PBIneq ![3,6,6,0] xs 7      := by apply Addition t2 t3
   exact Division t4 h3z
   done
+
+def resolved : ∃xs, PBIneq ![1,2,2,0] xs 3 :=
+  let xs := ![1,0,1,0]
+  let c1 : PBIneq ![1,2,1,0] xs 2 := by reduce ; exact Int.NonNeg.mk 0
+  let c2 : PBIneq ![1,2,4,2] xs 5 := by reduce ; exact c1
+  let c3 : PBIneq ![0,0,0,-1] xs (-1) := by reduce ; exact Int.NonNeg.mk 1
+  ⟨xs,derived xs c1 c2 c3⟩
 
 def main : IO Unit := do
   IO.println "This is lean-cutting-planes"
