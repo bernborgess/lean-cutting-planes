c4af013376457593761088d238c7d2b9dac60fd1
bernborgess <bernborgess@outlook.com>
2024-04-25 20:13:41 +0000

    Proof with Pi.add_apply and Finset.sum_add_distrib

--- LeanCuttingPlanes/Rules/Addition.lean
+++ LeanCuttingPlanes/Rules/Addition.lean
@@ -2,33 +2,6 @@ import «LeanCuttingPlanes».Data.PseudoBoolean
 
 namespace PseudoBoolean
 
-lemma singletonFinVec {a b : ℤ}: ![a] + ![b] = ![a+b] := by
-  exact List.ofFn_inj.mp rfl
-  done
-
-lemma twoElementFinVec {a b c d : ℤ} : ![a,b] + ![c,d] = ![a+c,b+d] := by
-  exact List.ofFn_inj.mp rfl
-  done
-
-open BigOperators
-
--- lemma basePart
---   (ha : PBIneq ![] ![] A)
---   (hb : PBIneq ![] ![] B)
---   : PBIneq ![] ![] (A + B) := by
---   have hk : PBSum ![] ![] = 0 := rfl
---   sorry
---   done
-
-lemma expandDefinitionOfPBIneq
- : (PBIneq cs xs const) = (PBSum cs xs ≥ const) := by rfl
-
--- It's literally the definition of the function...
-lemma expandDefinitionOfPBSum
-  : (PBSum cs xs) = (∑ i, (cs i + xs i)) := by
-  sorry
-  done
-
 -- Addition
 -- ∑i (a i * l i) ≥ A
 -- ∑i (b i * l i) ≥ B
@@ -39,15 +12,9 @@ theorem Addition
   {as : Fin n → ℤ} {A : ℤ} (ha : PBIneq as xs A)
   {bs : Fin n → ℤ} {B : ℤ} (hb : PBIneq bs xs B)
   : PBIneq (as + bs) xs (A + B) := by
-  induction n
-  .
-    rw [expandDefinitionOfPBIneq] at *
-    rw [expandDefinitionOfPBSum] at *
-    have sumZero : PBSum ![] ![] = 0 := rfl
-    simp [sumZero] at *
-    exact Int.add_nonpos ha hb
-
-  . sorry
+  rw [PBIneq,PBSum] at *
+  simp_rw [Pi.add_apply,add_mul,Finset.sum_add_distrib]
+  exact Int.add_le_add ha hb
   done
 
 example
