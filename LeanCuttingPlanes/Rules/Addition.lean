6e4c31f7cbc7ee15ef6e1c840ef52de598c9597d
bernborgess <bernborgess@outlook.com>
2024-04-26 08:51:10 -0300

    Commenting and explaining the proof

--- LeanCuttingPlanes/Rules/Addition.lean
+++ LeanCuttingPlanes/Rules/Addition.lean
@@ -13,8 +13,22 @@ theorem Addition
   {bs : Fin n → ℤ} {B : ℤ} (hb : PBIneq bs xs B)
   : PBIneq (as + bs) xs (A + B) := by
   rw [PBIneq,PBSum] at *
-  simp_rw [Pi.add_apply,add_mul,Finset.sum_add_distrib]
-  exact Int.add_le_add ha hb
+  -- ⊢ (∑i, (as + bs) i * (xs i)) ≥ A + B
+
+  simp [Pi.add_apply]           -- (x + y) i = x i + y i
+  -- ⊢ A + B ≤ ∑i, (as i + bs i) * xs i
+
+  simp [add_mul]                -- (a + b) * c = a * c + b * c
+  -- ⊢ A + B ≤ ∑i, as i * xs i + bs i * xs i
+
+  simp [Finset.sum_add_distrib] -- ∑i,(f i + g i) = (∑i, f i) + (∑i, g i)
+  -- ⊢ A + B ≤ (∑i, as i * xs i) + (∑i, bs i * xs i)
+
+  -- Using the hypotheses:
+  -- (ha : A ≤ ∑i, as i * xs i)
+  -- (hb : B ≤ ∑i, bs i * xs i)
+  exact Int.add_le_add ha hb    -- (a ≤ b) ∧ (c ≤ d) → a + c ≤ b + d
+  -- A + B ≤ (∑i, as i * xs i) + (∑i, bs i * xs i)
   done
 
 example
