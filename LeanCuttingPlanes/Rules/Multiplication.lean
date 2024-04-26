ac0f107786529020decefc3773f65eda28d04f4d
bernborgess <bernborgess@outlook.com>
2024-04-26 19:39:50 -0300

    Added proof comments

--- LeanCuttingPlanes/Rules/Multiplication.lean
+++ LeanCuttingPlanes/Rules/Multiplication.lean
@@ -4,18 +4,6 @@ namespace PseudoBoolean
 
 open BigOperators
 
-lemma lil
-  {a b c : ℤ}
-  : (a * b * c) = (a * (b * c)) := by
-  exact Int.mul_assoc a b c
-  done
-
-lemma sub1
-  {as xs : Fin n → ℤ}
-  : (∑ x : Fin n, c * as x * (xs x)) = (∑ x : Fin n, c * (as x * (xs x))) := by
-  simp [lil]
-  done
-
 -- Multiplication
 -- ∑i (a i * l i) ≥ A
 -- c : ℕ
@@ -25,21 +13,31 @@ lemma sub1
 theorem Multiplication
   {xs : Fin n → Fin 2}
   {as : Fin n → ℤ} {A : ℤ} (ha : PBIneq as xs A)
+  -- TODO: change `c` to ℕ
   {c : ℤ} (hc0 : c > 0)
   : PBIneq (c * as : Fin n → ℤ) xs (c * A) := by
   rw [PBIneq,PBSum] at *
-  simp [Pi.smul_apply]
-  rw [sub1]
+  -- ∑i, (c * as) i * (xs i) ≥ c * A
 
-  rw [Finset.mul_sum Finset.univ (λ i ↦ as i * xs i) c |>.symm]
-  apply mul_le_mul_iff_of_pos_left hc0 |>.mpr
+  simp [Pi.smul_apply]                              -- (c * as) i = c * (as i)
+  -- c * A ≤ ∑i, c * as i * (xs i)
+
+  simp [Int.mul_assoc]                              -- (a * b) * c = a * (b * c)
+  -- c * A ≤ ∑i, c * (as i * xs i)
+
+  rw [Finset.mul_sum Finset.univ (λ i ↦ as i * xs i) c |>.symm] -- ∑i, c * f i = c * ∑i, f i
+  -- c * A ≤ c * ∑i, as i * xs i
+
+  apply mul_le_mul_iff_of_pos_left hc0 |>.mpr       -- (c * A ≤ c * B) ∧ (c > 0) → (A ≤ B)
+  -- A ≤ ∑i, as i * xs i
+
   exact ha
   done
 
 example
   (ha : PBIneq ![1,0] xs 3)
   : PBIneq ![2,0] xs 6 := by
-  let h2z : (2:ℤ) > 0 := by exact (Int.sign_eq_one_iff_pos 2).mp rfl
+  let h2z : (2:ℤ) > 0 := by exact Int.sign_eq_one_iff_pos 2 |>.mp rfl
   apply Multiplication ha h2z
   done
 
