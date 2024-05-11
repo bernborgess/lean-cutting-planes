aea5e6be476d8b6bda7432724aa52cecb5330ebf
bernborgess <bernborgess@outlook.com>
2024-05-11 14:57:47 -0300

    Normal Form PBO Notation

--- LeanCuttingPlanes/Data/PBO.lean
+++ LeanCuttingPlanes/Data/PBO.lean
@@ -1,28 +1,29 @@
 import Mathlib.Algebra.BigOperators.Basic
 import Mathlib.Data.Fin.Tuple.Reflection
 import Mathlib.Data.Fin.VecNotation
+import Mathlib.Data.Matrix.Notation
 
 namespace PseudoBoolean
 
 open FinVec BigOperators
 
-def PBSum (cs : Fin n → ℤ) (xs : Fin n → Fin 2) :=
-  ∑ i, (cs i * xs i)
+def PBSum (cs : Matrix (Fin n) (Fin 2) ℕ) (xs : Fin n → Fin 2) :=
+  ∑ i, ((cs i) 0 * xs i + (cs i) 1 * (1 - xs i))
 
-def PBIneq {n : ℕ} (cs : Fin n → ℤ) (xs : Fin n → Fin 2) (const : ℤ) :=
+def PBIneq {n : ℕ} (cs : Matrix (Fin n) (Fin 2) ℕ) (xs : Fin n → Fin 2) (const : ℤ) :=
   PBSum cs xs ≥ const
 
-example : PBIneq ![1] ![1] 1 := by
+example : PBIneq !![1,0] ![1] 1 := by
   reduce                -- Expand the goal to 1 * 1 ≥ 1
   exact Int.le_refl 1   -- Prove that 1 * 1 ≥ 1
   done
 
-example : PBIneq ![1,2] ![0,1] 2 := by
+example : PBIneq !![1,0;2,0] ![0,1] 2 := by
   reduce                -- Change goal to 1 * 0 + 2 * 1 ≥ 2
   exact Int.le_refl 2   -- Prove 1 * 0 + 2 * 1 ≥ 2
   done
 
-example : PBIneq ![3,4] ![0,1] 2 := by
+example : PBIneq !![3,0;4,0] ![0,1] 2 := by
   reduce
   exact Int.NonNeg.mk 2
   done
