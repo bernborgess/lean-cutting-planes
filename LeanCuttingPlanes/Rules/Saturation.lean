ac72ea990502541e5a087375cbc5fa8a3e0529a3
bernborgess <bernborgess@outlook.com>
2024-05-11 15:01:42 -0300

    NF Saturation rewrite

--- LeanCuttingPlanes/Rules/Saturation.lean
+++ LeanCuttingPlanes/Rules/Saturation.lean
@@ -1,7 +1,7 @@
 import «LeanCuttingPlanes».Data.PBO
 
 namespace PseudoBoolean
-open FinVec
+open FinVec Matrix
 
 -- Saturation
 -- ∑i (a i * l i) ≥ A
@@ -9,12 +9,13 @@ open FinVec
 -- ∑i ( min(a i, A) * l i) ≥ A
 theorem Saturation
   {xs : Fin n → Fin 2}
-  {as : Fin n → ℤ} {A : ℤ} (ha : PBIneq as xs A)
-  : PBIneq (map (min A) as) xs A := sorry
+  {as : Matrix (Fin n) (Fin 2) ℕ} {A : ℕ} (ha : PBIneq as xs A)
+  : PBIneq (as.map (min A)) xs A :=
+  sorry
 
 example
-  (ha : PBIneq ![3,4] xs 3)
-  : PBIneq ![3,3] xs 3 := by
+  (ha : PBIneq !![3,4] xs 3)
+  : PBIneq !![3,3] xs 3 := by
   apply Saturation ha
   done
 
