9874216816b78416cea52db831a9bc56cf521ab2
bernborgess <bernborgess@outlook.com>
2024-05-11 14:57:58 -0300

    NF Addition rewrite

--- LeanCuttingPlanes/Rules/Addition.lean
+++ LeanCuttingPlanes/Rules/Addition.lean
@@ -2,6 +2,8 @@ import «LeanCuttingPlanes».Data.PBO
 
 namespace PseudoBoolean
 
+open BigOperators
+
 -- Addition
 -- ∑i (a i * l i) ≥ A
 -- ∑i (b i * l i) ≥ B
@@ -9,8 +11,8 @@ namespace PseudoBoolean
 -- ∑i ((a i + b i) * l i) ≥ A + B
 theorem Addition
   {xs : Fin n → Fin 2}
-  {as : Fin n → ℤ} {A : ℤ} (ha : PBIneq as xs A)
-  {bs : Fin n → ℤ} {B : ℤ} (hb : PBIneq bs xs B)
+  {as : Fin n → Fin 2 → ℕ} {A : ℕ} (ha : PBIneq as xs A)
+  {bs : Fin n → Fin 2 → ℕ} {B : ℕ} (hb : PBIneq bs xs B)
   : PBIneq (as + bs) xs (A + B) := by
   rw [PBIneq,PBSum] at *
   -- ⊢ (∑i, (as + bs) i * (xs i)) ≥ A + B
@@ -24,17 +26,18 @@ theorem Addition
   simp [Finset.sum_add_distrib] -- ∑i,(f i + g i) = (∑i, f i) + (∑i, g i)
   -- ⊢ A + B ≤ (∑i, as i * xs i) + (∑i, bs i * xs i)
 
+  sorry
   -- Using the hypotheses:
   -- (ha : A ≤ ∑i, as i * xs i)
   -- (hb : B ≤ ∑i, bs i * xs i)
-  exact Int.add_le_add ha hb    -- (a ≤ b) ∧ (c ≤ d) → a + c ≤ b + d
+  -- exact Int.add_le_add ha hb    -- (a ≤ b) ∧ (c ≤ d) → a + c ≤ b + d
   -- A + B ≤ (∑i, as i * xs i) + (∑i, bs i * xs i)
   done
 
 example
-  (ha : PBIneq ![1,0] xs 1)
-  (hb : PBIneq ![1,1] xs 2)
-  : PBIneq ![2,1] xs 3 := by
+  (ha : PBIneq !![1,0;0,0] xs 1)
+  (hb : PBIneq !![1,0;1,0] xs 2)
+  : PBIneq !![2,0;1,0] xs 3 := by
   apply Addition ha hb
   done
 
