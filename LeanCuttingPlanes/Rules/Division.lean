17dc91ded793d88f7ace92585ca54674734c7a09
bernborgess <bernborgess@outlook.com>
2024-05-27 11:36:54 -0300

    Split Division proof into lemmas

--- LeanCuttingPlanes/Rules/Division.lean
+++ LeanCuttingPlanes/Rules/Division.lean
@@ -1,9 +1,24 @@
 import «LeanCuttingPlanes».Basic
+import Mathlib.Algebra.Order.Floor.Div
 
 namespace PseudoBoolean
-open FinVec Matrix BigOperators
+open FinVec Matrix BigOperators Finset
 
-def ceildiv (c : ℕ) (a : ℕ) := (a+c-1) / c
+def ceildiv (c : ℕ) (a : ℕ) := a ⌈/⌉ c
+
+lemma ceildiv_le_ceildiv_left
+  {a b : ℕ}
+  (c : ℕ)
+  (hab : a ≤ b)
+  : a ⌈/⌉ c ≤ b ⌈/⌉ c := sorry
+
+lemma sum_ceildiv (as : Fin n → ℕ) (c : ℕ)
+  : (∑i, as i) ⌈/⌉ c = ∑i,(as i ⌈/⌉ c)
+  := sorry
+
+lemma ceildiv_ite (P : Prop) [Decidable P] (a b c : ℕ) :
+  (if P then b else c) ⌈/⌉ a = if P then b ⌈/⌉ a else c ⌈/⌉ a
+  := sorry
 
 -- Division
 -- ∑i (a i * l i) ≥ A
@@ -17,15 +32,11 @@ theorem Division
   {c : ℕ} (hc0 : c > 0)
   : PBIneq (map (mapBoth (ceildiv c)) as) xs (ceildiv c A) := by
   unfold PBIneq PBSum ceildiv mapBoth at *
-  simp at *
-  /-
-  (A + c - 1) / c ≤
-    ∑ x : Fin n,
-      if xs x = 1
-      then ((as x).1 + c - 1) / c
-      else ((as x).2 + c - 1) / c
-  -/
-  sorry
+  simp only [Fin.isValue, ge_iff_le, gt_iff_lt, Prod_map, map_eq, Function.comp_apply] at *
+  apply ceildiv_le_ceildiv_left c at ha
+  rw [sum_ceildiv] at ha
+  simp only [ceildiv_ite] at ha
+  exact ha
   done
 
 example
